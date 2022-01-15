dfdd8fc0cabf6376e7ad51707f4642227b6d25e6
BetaHuhn <schiller@mxis.ch>
2022-01-15 22:10:34 +0100

    🐛 Fix icon import

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,8 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   srcDir: 'docs/',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Vue Command Menu',
     htmlAttrs: {
@@ -20,33 +18,27 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '@blokwise/dynamic'
   ],
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  telemetry: false
 }
